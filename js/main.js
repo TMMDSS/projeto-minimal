ed0de617bf6780ea2f914311a6d68282a50c1086
TMMDSS <souzatailam@gmail.com>
2023-03-03 18:50:09 -0300

    JavaScript adcionado com sucesso

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function clickdesc1() {
+    if (descca1.style.display == 'block') {
+        descca1.style.display = 'none'
+    } else {
+        descca1.style.display = 'block'
+    }
+}
+
+function clickdesc2() {
+    if (descca2.style.display == 'block') {
+        descca2.style.display = 'none'
+    } else {
+        descca2.style.display = 'block'
+    }
+}
+
+function clickdesc3() {
+    if (descca3.style.display == 'block') {
+        descca3.style.display = 'none'
+    } else {
+        descca3.style.display = 'block'
+    }
+}
+
+function clickdesc4() {
+    if (descca4.style.display == 'block') {
+        descca4.style.display = 'none'
+    } else {
+        descca4.style.display = 'block'
+    }
+}
